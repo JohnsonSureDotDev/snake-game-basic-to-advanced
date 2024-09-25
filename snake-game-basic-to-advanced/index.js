2138deb3fa368d44060709e0504857bf7e6bdf3a
JohnsonSureDotDev <msure0012@gmail.com>
2024-09-24 17:05:13 -0700

    Update index.js

--- snake-game-basic-to-advanced/index.js
+++ snake-game-basic-to-advanced/index.js
@@ -4,18 +4,31 @@ snake.headPosition=90
 snake.length=4
 snake.speed=100
 snake.virtualHeadPos=0
+snake.headTrail={}
+snake.headTrail.position=[]
+snake.headTrail.increment=[]
 var snakeGameOption="classic"
 var numberOfGrids=100
-var gridOfreference=0
+var tileOfreference=0
+var incrementOrDecrement=1
 function gameloop(){
 setInterval(() => { 
+    snake.headTrail.position.push(snake.headPosition)
     var snakeTailPos;
-   
+   box[snake.headPosition].style.background="red"
     snakeTailPos=snake.headPosition-snake.length
-    //var virtualSnakeHead=snake.headPosition
-    box[snake.headPosition].style.background="red"
-    snakeTailPos>=gridOfreference?box[snakeTailPos].style.background="transparent":""
-console.log(snakeTailPos) 
+    
+    //code below is for colouring every tile that the snake tail passes through
+    console.log(snake.headTrail.position)
+    snakeTailPos>=tileOfreference?box[function (){
+    
+        
+           return  snake.headTrail.position[snake.headTrail.position.length-snake.length]
+             // snake.headTrail.position.pop(snake.headTrail.position[snake.headTrail.position.length-1])
+        snakeTailPos
+    }()].style.background="transparent":""
+
+    //code below is for snake head to loop one end to another
 if(snake.headPosition>snake.length&&(snake.headPosition+1)%(numberOfGrids/10)===0){
     if(snakeGameOption==="classic"){
          snake.virtualHeadPos=snake.headPosition
@@ -25,8 +38,11 @@ if(snake.headPosition>snake.length&&(snake.headPosition+1)%(numberOfGrids/10)===
     }
    
 }
-    snake.headPosition++ 
-    
+
+snake.headTrail.increment.push(incrementOrDecrement)
+    snake.headPosition+=incrementOrDecrement
+    console.log(snake.headTrail.position)
+    console.log(snake.headTrail.increment)
 
 
     
